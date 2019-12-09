85f2937c76368dce1161416fa403595c9693fa7a
ricky42613 <ricky42613@gmail.com>
2019-12-09 13:17:57 +0800

    init

---
+++ socket_client.c
@@ -0,0 +1,357 @@
+/*************************************************************************** * * Copyright (c) 2016 Lubanr.com All Rights Reserved * **************************************************************************/
+
+
+
+/** * @file client.c * @date 2016/11/22 18:40:52 * @version $Revision$ * @brief * **/
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+#ifndef WIN32
+#include <netinet/in.h>
+# ifdef _XOPEN_SOURCE_EXTENDED
+# include <arpa/inet.h>
+# endif
+#include <sys/socket.h>
+#endif
+
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+#include <event2/event.h>
+
+#define EVENT_BASE_SOCKET 0
+
+static const int PORT = 9876;
+const char* server_ip = "127.0.0.1";
+char user[100]={0};
+int status = 0;//1:o 2:x
+int player2 = 0;
+int board[3][3]={{0,0,0},{0,0,0},{0,0,0}};
+int chi_cnt = 0;
+int turn = 0; //1:ok 2:not
+/*事件处理回调函数*/
+int check_table[8][3] ={{1,2,3},{1,4,7},{1,5,9},{2,5,8},{3,5,7},{3,6,9},{4,5,6},{7,8,9}};
+int check_win(int a[3][3]){
+	int my_pos[10]={0,0,0,0,0,0,0,0,0,0};
+	int id = 0;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			id++;
+			if(a[i][j] == status){
+				my_pos[id] = 1;	
+			}	
+		}
+	}
+	for(int i=0;i<8;i++){
+		int first=check_table[i][0],second=check_table[i][1],third=check_table[i][2];
+		if( my_pos[first]==1&& my_pos[second]==1 && my_pos[third]==1 ){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void print_board(int a[3][3]){
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if(board[i][j] == 1)
+				printf("o");
+			if(board[i][j] == 2)
+				printf("x");
+			if(board[i][j] == 0)
+				printf(" ");
+			if(j!=2){
+				printf("|");
+			}else{
+				printf("\n");
+			}
+		}
+		if(i!=2){
+			printf("------\n");
+		}
+	}
+}
+
+void event_cb(struct bufferevent* bev,short events,void* ptr)
+{
+	if(events & BEV_EVENT_CONNECTED)//连接建立成功
+	{
+		printf("connected to server successed!\n");
+	}
+	else if(events & BEV_EVENT_ERROR)
+	{
+		printf("connect error happened!\n");
+	}
+}
+
+void read_cb(struct bufferevent *bev, void *ctx)
+{
+	struct evbuffer* input = bufferevent_get_input(bev);
+	size_t len = 0;
+	len = evbuffer_get_length(input);
+	// read
+	char* buf;
+	buf = (char*)malloc(sizeof(char)*len);
+	if(NULL==buf){return;}
+	evbuffer_remove(input,buf,len);
+	if(strcmp(buf,"sorry\0")==0){
+		printf("plz wait a minute\n");
+		exit(1);
+	}
+	else if(strncmp(buf,"logout",6)==0){
+		memset(user,'\0',sizeof(char));
+		printf("logout success!\n");
+	}
+	else if(strncmp(buf,"hi ",3)==0){
+		char *ptr = buf+3;
+		strcpy(user,ptr);
+		printf("server:\n%s\r\n",buf);
+	}
+	else if(strncmp(buf,"invite msg from:",16)==0){
+		printf("server:%s\n",buf);
+		char ans,*ptr,str[1024]="answer:\0";
+		if(buf[len-1]=='\n'){
+			buf[len-1] = '\0';
+		}
+		ptr = buf+16;
+		scanf("%c",&ans);
+		str[7] = ans;
+		str[8] = '\0';
+		strcat(str,ptr);
+		bufferevent_write(bev,str,strlen(str));	 
+	}
+	else if(strncmp(buf,"at:",3)==0){
+		char *ptr = buf+7;
+		int x = atoi(buf+3),y=atoi(buf+5);
+		turn = 1;
+		board[x-1][y-1] = player2;
+		printf("%s\n",buf);
+		if(strstr(buf,"lose")!=NULL){
+			print_board(board);
+			printf("\nyou lose\nleave game\n");
+			player2 = 0;
+			status = 0;
+			turn = 0;
+			for(int i=0;i<3;i++){
+				for(int j=0;j<3;j++){ 
+					board[i][j] = 0;
+				}
+			}
+			chi_cnt = 0;
+			return;
+
+		}
+		else if(strstr(buf,"no one win")!=NULL){
+			print_board(board);
+			printf("\nno one win the game\nleave game\n");
+			player2 = 0;
+			status = 0;
+			turn = 0;
+			for(int i=0;i<3;i++){
+				for(int j=0;j<3;j++){
+					board[i][j] = 0;
+				}
+			}
+			chi_cnt = 0;
+			return;
+		}
+		chi_cnt++;
+		printf("your turn\n");
+		print_board(board);
+	}
+	else{
+		if(strncmp(buf,"start",5)==0){
+			if(strlen(buf)<8){
+				status = 2;
+				player2 = 1;
+				turn = 2;
+			}
+			else{
+				status = 1;
+				player2 = 2;
+				turn = 1;
+			}
+		} 
+		printf("server:\n%s\r\n",buf);
+	}
+}
+
+int tcp_connect_server(const char* server_ip,int port)
+{
+	struct sockaddr_in server_addr;
+	int status = -1;
+	int sockfd;
+
+	server_addr.sin_family = AF_INET;  
+	server_addr.sin_port = htons(port);  
+	status = inet_aton(server_ip, &server_addr.sin_addr);
+	if(0 == status)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);  
+	if( sockfd == -1 )  
+		return sockfd;  
+
+
+	status = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr) );  
+
+	if( status == -1 )  
+	{
+		close(sockfd);
+		return -1;  
+	}  
+
+	evutil_make_socket_nonblocking(sockfd);
+
+	return sockfd;
+}
+
+void cmd_msg_cb(int fd, short events, void *arg)
+{
+	char msg[1024];
+	int ret = read(fd,msg,sizeof(msg));
+	if(ret < 0 )
+	{
+		perror("read failed");
+		exit(1);
+	}
+
+	struct bufferevent* bev = (struct bufferevent*)arg;
+
+	msg[ret] = '\0';
+	if(strncmp(msg,"at:",3)!=0&&status!=0){
+		printf("you are in the game!\n");
+		return;
+	}
+	if(strncmp(msg,"acn:",4)==0){
+		if(strlen(user)){
+			printf("system:plz logout first\n");
+			return;
+		}
+	}
+	if(strcmp(msg,"logout")==0){
+		if(strlen(user)==0){
+			printf("u r not login\n");
+			return;
+		}
+	}
+	if(strncmp(msg,"at:",3)==0){
+		int flag=0;
+		int x = atoi(msg+3),y=atoi(msg+5);
+		if(turn!=1){
+			printf("forbidden\n");
+			return;
+		}
+		if( x<1 || x>3 || y<1 || y>3 ){
+			printf("forbidden!\n");
+			return;
+		} 
+		else if(board[x-1][y-1]!=0){
+			printf("forbidden!\n");
+			return;
+		}
+		else{
+			board[x-1][y-1] = status;
+			turn = 2;
+			chi_cnt++;
+			if(check_win(board)){
+				if(msg[strlen(msg)-1] == '\n')
+					msg[strlen(msg)-1] = '\0';
+				flag=1;
+				print_board(board);
+				printf("you win\n");
+				player2 = 0;
+				status = 0;
+				turn = 0;
+				for(int i=0;i<3;i++){
+					for(int j=0;j<3;j++){ 
+						board[i][j] = 0;
+					}
+				}
+				chi_cnt = 0;
+				strcat(msg,"you lose!over\n");
+				//printf("%s\n",msg);
+			}
+			else if(chi_cnt == 9){
+				if(msg[strlen(msg)-1] == '\n')
+                                	msg[strlen(msg)-1] = '\0';
+				flag=1;
+				print_board(board);
+				printf("no one win the game!\n");
+				strcat(msg,"no one win the game!over\n");
+				//printf("%s\n",msg);
+				player2 = 0;
+				status = 0;
+				turn = 0;
+				for(int i=0;i<3;i++){
+					for(int j=0;j<3;j++){
+						board[i][j] = 0;
+					}
+				}
+				chi_cnt = 0;
+			}
+		}
+		if(flag==0)
+			print_board(board);
+	}
+	//把终端消息发给服务器段
+	bufferevent_write(bev,msg,strlen(msg));
+}
+
+int main()
+{
+	struct event_base* base = NULL;
+	struct sockaddr_in server_addr;
+	struct bufferevent *bev = NULL;
+	int status;
+	int sockfd;
+
+	//申请event_base对象
+	base = event_base_new();
+	if(!base)
+	{
+		printf("event_base_new() function called error!");
+	}
+
+	//初始化server_addr
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port= htons(PORT);
+	status = inet_aton(server_ip, &server_addr.sin_addr);
+
+#if EVENT_BASE_SOCKET
+
+	sockfd = tcp_connect_server(server_ip,PORT);
+	bev = bufferevent_socket_new(base, sockfd,BEV_OPT_CLOSE_ON_FREE);
+#else
+	bev = bufferevent_socket_new(base, -1,BEV_OPT_CLOSE_ON_FREE);//第二个参数传-1,表示以后设置文件描述符
+
+	//调用bufferevent_socket_connect函数
+	bufferevent_socket_connect(bev,(struct sockaddr*)&server_addr,sizeof(server_addr));
+#endif
+	//监听终端的输入事件
+	struct event* ev_cmd = event_new(base, STDIN_FILENO,EV_READ | EV_PERSIST, cmd_msg_cb, (void*)bev);  
+
+	//添加终端输入事件
+	event_add(ev_cmd, NULL);
+
+	//设置bufferevent各回调函数
+	bufferevent_setcb(bev,read_cb, NULL, event_cb, (void*)NULL);  
+
+	//启用读取或者写入事件
+	bufferevent_enable(bev, EV_READ | EV_PERSIST);
+
+	//开始事件管理器循环
+	event_base_dispatch(base);
+
+	event_base_free(base);
+	return 0;
+
+}
---
+++ socket_server.c
@@ -0,0 +1,261 @@
+#include <event2/event.h>
+#include <event2/listener.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <netinet/tcp.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int cli_cnt = 0;
+int cli_status[4]={0,0,0,0}; //0:free,1:handle invite,2:gaming 
+int cli_id[4]={-1,-1,-1,-1};
+char cli_acn[4][4]={"aaa\0","bbb\0","ccc\0","ddd\0"};
+char cli_pwd[4][4]={"aaa\0","bbb\0","ccc\0","ddd\0"};
+int b_info[2][4] = {{0,0,0,0},{0,0,0,0}};
+//struct bufferevent cli[4];  
+
+static void server_on_read(struct bufferevent* bev,void* arg){
+	struct evbuffer* input = bufferevent_get_input(bev);
+	size_t len = 0;
+	len = evbuffer_get_length(input);
+
+	// read
+	char* buf;
+	buf = (char*)malloc(sizeof(char)*len);
+	if(NULL==buf){return;}
+	evbuffer_remove(input,buf,len); // clear the buffer
+	evutil_socket_t fd = bufferevent_getfd(bev);  
+	printf("%s",buf);
+	if(strcmp(buf,"ls\n")==0){
+		int login_flag=0;
+		for(int i=0;i<4;i++){
+			if(cli_id[i]==(int)fd){
+				login_flag=1;
+			}
+		}
+		if(login_flag){
+			char idlist[1024]={0};
+			for(int i=0;i<4;i++){
+				if(cli_id[i]!=-1){
+					strcat(idlist,cli_acn[i]);
+					strcat(idlist,"\n");
+				}
+			}
+			bufferevent_write(bev,idlist,strlen(idlist));
+		}
+		else{
+			bufferevent_write(bev,"login first",12);	
+		}
+	}
+	if(strncmp(buf,"acn:",4)==0){
+		if(cli_cnt<4){
+			char *ptr=buf+4,usr_acn[1024],usr_pwd[1024];
+			int flag=0,len=0;
+			while(*ptr!='\n'){
+				if(strncmp(ptr,",pwd:",5)==0){
+					usr_acn[len] = '\0';
+					ptr = ptr + 5;
+					len=0;
+					flag=1;	
+				}
+				if(flag){
+					usr_pwd[len] = *ptr;
+					len++;
+					ptr++;
+				}else{
+					usr_acn[len] = *ptr;
+					len++;
+					ptr++;
+				}
+			}
+			usr_pwd[len] = '\0';
+			int flag2=-1;
+			for(int i=0;i<4;i++){
+				if(cli_id[i]==-1){
+					if(strcmp(cli_acn[i],usr_acn)==0&&strcmp(cli_pwd[i],usr_pwd)==0){
+						cli_id[i] = (int)fd;
+						flag2 = i;
+						cli_cnt++; 
+						break;
+					}
+				}
+			}
+			if(flag2!=-1){
+				char str[10] = "hi ";
+				bufferevent_write(bev,strcat(str,cli_acn[flag2]),7);	
+			}
+			else{
+				bufferevent_write(bev,"fail",5);
+			}
+		}
+		else{
+			bufferevent_write(bev,"sorry\0",5);
+		}
+	}
+	if(strncmp("invite:",buf,7)==0){
+		if(buf[strlen(buf)-1] == '\n')
+			buf[strlen(buf)-1] = '\0';
+		char *ptr = buf+7;
+		int from = -1,target=-1;
+		for(int i=0;i<4;i++){
+			if(fd == cli_id[i]){
+				from = i;
+			}
+		}
+		if(from==-1){
+			bufferevent_write(bev,"login first\0",12);
+		}else{	
+			int flag = -1;
+			for(int i=0;i<4;i++){
+				if(-1 != cli_id[i]&&strcmp(ptr,cli_acn[i])==0){
+					target = cli_id[i];
+					if(cli_status[i] !=0){
+						flag = i;
+					}
+				}
+			}
+			if(flag!=-1){
+				bufferevent_write(bev,"player is busy",15);
+			}
+			else if(target!=-1){
+				char tmp[1024] = "invite msg from:";
+				int tmplen = strlen(cli_acn[from])+strlen(tmp);
+				cli_status[flag] = 1;
+				cli_status[from] = 1;
+				write(target,strcat(tmp,cli_acn[from]),tmplen);
+			}else{
+				bufferevent_write(bev,"player does not exist\0",21);
+			}
+		}
+
+	}
+	if(strncmp("answer:",buf,7)==0){
+		if(buf[strlen(buf)-1]=='\n'){
+			buf[strlen(buf)-1] = '\0';
+		}
+		char *ptr = buf+8;
+		char ans = buf[7];
+		int b_id = -1;
+		int target,idx1,idx2;
+		for(int i=0;i<4;i++){
+			if(strcmp(ptr,cli_acn[i])==0){
+				target = cli_id[i];
+				idx1 = i;
+			}
+			if(fd == cli_id[i]){
+				idx2 = i;
+			}
+		} 		
+		if(ans=='y'){
+			if(b_info[0][0]==0){
+				b_id = 0;	
+			}else{
+				b_id = 1;
+			}
+			cli_status[idx1] = 2;
+			cli_status[idx2] = 2;
+			b_info[b_id][0] = 1;
+			b_info[b_id][1] = target;
+			b_info[b_id][2] = target;
+			b_info[b_id][3] = fd;
+			write(target,"start\nit's your turn\n",22);
+			bufferevent_write(bev,"start\n\0",7);
+		}
+		else if(ans=='n'){
+			cli_status[idx1] = 0;
+			cli_status[idx2] = 0;
+			write(target,"invite is rejected",19);
+		}
+	}
+	else if(strncmp("at:",buf,3)==0){
+		int b_id;
+		if(fd == b_info[0][2]||fd == b_info[0][3]){
+			b_id = 0;
+		}
+		else{
+			b_id = 1;
+		}
+		if(b_info[b_id][1]!=fd){
+			bufferevent_write(bev,"not your turn!",15);
+		}else{
+			int b_me = b_info[b_id][2];
+			b_info[b_id][1] = b_info[b_id][3];
+			if(b_me!=fd){
+				b_me = b_info[b_id][3];
+				b_info[b_id][1] = b_info[b_id][2];
+			}
+			write(b_info[b_id][1],buf,strlen(buf));
+		}
+		if(strstr(buf,"over")!=NULL){
+			int p1=b_info[b_id][2],p2=b_info[b_id][3];
+			for(int i=0;i<4;i++){
+				b_info[b_id][i] = 0;
+			}
+			for(int i=0;i<4;i++){
+				if(cli_id[i]==p1||cli_id[i]==p2){
+					cli_status[i] = 0;
+				}
+			}
+		}
+
+	}
+	else if(strncmp(buf,"logout",6)==0){
+		for(int i=0;i<4;i++){
+			if(cli_id[i]==fd){
+				cli_id[i]=-1;
+				cli_cnt --;
+			}
+			bufferevent_write(bev,"logout",7);
+		}	
+	}
+	free(buf);
+	buf=NULL;
+	return;
+}
+
+
+void server_on_accept(struct evconnlistener* listener,evutil_socket_t fd,struct sockaddr *address,int socklen,void *arg)
+{
+	printf("accept a client : %d\n", fd);
+	// set TCP_NODELAY
+	if(cli_cnt<4){
+		int enable = 1;
+		if(setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, (void*)&enable, sizeof(enable)) < 0)
+			printf("Consensus-side: TCP_NODELAY SETTING ERROR!\n");
+		struct event_base *base = evconnlistener_get_base(listener);
+		struct bufferevent* new_buff_event = bufferevent_socket_new(base,fd,BEV_OPT_CLOSE_ON_FREE);
+		bufferevent_setcb(new_buff_event,server_on_read,NULL,NULL,NULL);
+		bufferevent_enable(new_buff_event,EV_READ|EV_WRITE);
+	}else{
+		char tmp[100]="sorry\0";
+		write((int)fd,tmp,strlen(tmp));
+	}
+	return;
+}
+
+int main()
+{
+	int port = 9876;
+	struct sockaddr_in my_address;
+	memset(&my_address, 0, sizeof(my_address));
+	my_address.sin_family = AF_INET;
+	my_address.sin_addr.s_addr = htonl(0x7f000001); // 127.0.0.1
+	my_address.sin_port = htons(port);
+
+	struct event_base* base = event_base_new();
+	struct evconnlistener* listener = 
+		evconnlistener_new_bind(base,server_on_accept,
+				NULL,LEV_OPT_CLOSE_ON_FREE|LEV_OPT_REUSEABLE,-1,
+				(struct sockaddr*)&my_address,sizeof(my_address));
+
+	if(!listener)
+		exit(1);
+
+	event_base_dispatch(base);
+	evconnlistener_free(listener);
+	event_base_free(base);
+
+	return 0;
+}
+
